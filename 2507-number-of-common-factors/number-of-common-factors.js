d3666dc5bb2676598d36635796e02e9efd318653
Safuvan K <safuvanvkm@gmail.com>
2025-10-12 16:17:30 +0530

    Time: 2 ms (10.29%) | Memory: 54.3 MB (41.16%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function(a, b) {
+    let n = Math.max(a,b);
+    let i = 0 ;
+    let result = [];
+    while(n>=i){
+        if(a%i=== 0 && b%i === 0){
+            result.push(i)
+        }i++;
+    }return result.length
+};

8aa2813a0224f421a3f90efedab0a2defdc1323e
Safuvan K <safuvanvkm@gmail.com>
2026-03-03 06:28:35 +0530

    Time: 0 ms (100.00%) | Memory: 58.1 MB (65.83%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -3,6 +3,15 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let ans = [...nums,...nums]
+   let ans = [];
+
+    for (let i = 0; i<nums.length ; i++){
+        ans[i] = nums[i]
+    }
+
+    for(let i = 0 ; i<nums.length ; i++){
+        ans[i + nums.length] = nums[i]
+    }
+
     return ans
 };
